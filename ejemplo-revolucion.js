da3f741e80ef177803c2cee7744b978fa109649d
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-02-23 13:42:27 -0600

    Update ejemplo-revolucion.js

--- ejemplo-revolucion.js
+++ ejemplo-revolucion.js
@@ -2,7 +2,7 @@ var puntos = [];
 
 for ( var i = 0; i < 50; i ++ ) {
     puntos.push( new THREE.Vector2(
-                     Math.sin( i * 0.2 ) * 15 + 50,
+                     Math.sin( i * 0.2 ) * 15 + 150,
                      ( i - 5 ) * 2 ) );
 }
 
