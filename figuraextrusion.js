5283d4172e3c1d49e83d6e573e247850a4b78413
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-02-28 14:29:55 -0600

    Update figuraextrusion.js

--- figuraextrusion.js
+++ figuraextrusion.js
@@ -31,8 +31,8 @@ var forma = new THREE.ExtrudeGeometry( figura,
                                        
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateY( Math.PI/4 );
-malla.rotateZ( Math.PI/4 );
+//malla.rotateY( Math.PI/4 );
+//malla.rotateZ( Math.PI/4 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
