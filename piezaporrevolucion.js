c53f008dc4ecfa46c97f5670b735d0db37b98c0b
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-02 01:03:12 -0600

    Update piezaporrevolucion.js

--- piezaporrevolucion.js
+++ piezaporrevolucion.js
@@ -18,7 +18,7 @@ var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( forma, material );
-//malla.rotateX( Math.PI/6 );
+malla.rotateX( Math.PI/4 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
