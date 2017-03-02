67dd150b85600f8c87e8ca397a33bcfd6e68fa15
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-01 23:46:25 -0600

    Create piezaporrevolucion.js

---
+++ piezaporrevolucion.js
@@ -0,0 +1,32 @@
+var puntos = [];
+
+for ( var i = 0; i < 10; i ++ ) {
+    puntos.push( new THREE.Vector2( (5/9)*i , i ) );
+}
+for ( var i = 10; i < 19; i ++ ) {
+    puntos.push( new THREE.Vector2( 8-(1/3)*i , i ) );
+}
+for ( var i = 19; i < 23; i ++ ) {
+    puntos.push( new THREE.Vector2( 2, i ) );
+}
+for ( var i = 23; i < 29; i ++ ) {
+    puntos.push( new THREE.Vector2( (16-(i-25)^2)^(1/2) , i ) );
+}
+
+var forma = new THREE.LatheGeometry(puntos);
+
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/6 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
