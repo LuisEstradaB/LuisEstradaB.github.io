cdac45a7f0c75abeb6dc62e804619be2e007ea13
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-03 00:09:46 -0600

    Update piezasjuntas.js

--- piezasjuntas.js
+++ piezasjuntas.js
@@ -94,9 +94,10 @@ pieza.merge(cuelloMalla.geometry,cuelloMalla.matrix);
 pieza.merge(cabezaMalla.geometry,cabezaMalla.matrix);
 pieza.translate(100,10,0);
 
-var color = new THREE.Color("rgb(255, 0, 0)");
+var c = new THREE.Color("rgb(255, 0, 0)");
 //var material3 = new THREE.MeshNormalMaterial();
-var material3 = new THREE.MeshBasicMaterial(color);
+var material3 = new THREE.MeshBasicMaterial();
+material3.color=c;
 
 var piezaMalla = new THREE.Mesh(pieza,material3);
 
