fb5eaadd363fd97fd946edf6264efa0a6bbfa321
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-16 13:39:21 -0600

    Update pract2.js

--- pract2.js
+++ pract2.js
@@ -148,7 +148,8 @@ var planoCercano = 1;
 var planoLejano = 1000;
 
 var camara = new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
-camara.position.y = 5;
+camara.position.z = 15;
+camara.position.y = 1;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
