1f185a9c58205162e9ad18be852b34e54aec20cc
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-21 14:17:31 -0600

    Create ejemploanimacion.js

---
+++ ejemploanimacion.js
@@ -0,0 +1,26 @@
+function setup() {
+  escena = new THREE.Scene();
+  camara = ne THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+  
+  camara.position.z = 5;
+  
+  renderer = new THREE.WEBGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  document.body.appendChild(renderer.domElement);
+  
+  malla = new THREE.Mesh( new THREE.SphereGeometry( 1 ), new THREE.MeshNormalMaterial() );
+  escena.add(malla);
+}
+
+function loop()[
+  requestAnimationFrame( loop );
+  
+  malla.rotation.x += 0.01;
+  malla.rotation.y += 0.01;
+  renderer.render( escena, camara );
+}
+
+var malla, camara, renderer, escena;
+
+setup();
+loop();
