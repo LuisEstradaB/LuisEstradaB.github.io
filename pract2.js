903a4355a23b8222b79e7956cee5a478c837cbfd
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-20 19:58:58 -0600

    Update pract2.js

--- pract2.js
+++ pract2.js
@@ -246,7 +246,7 @@ camara.right = window.innerWidth/2;
 camara.top = window.innerHeight/2;
 camara.bottom = window.innerHeight/-2;
 camara.near = 0.1;
-camara.far = 100;
+camara.far = 10;
 camara.updateProjectionMatrix();
 
 camara.position.z = 1;
