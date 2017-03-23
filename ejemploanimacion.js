6ad4faf38b0b9f3ea4f379095831bb19613fa08f
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-03-23 14:09:03 -0600

    Update ejemploanimacion.js

--- ejemploanimacion.js
+++ ejemploanimacion.js
@@ -5,9 +5,9 @@ function listener(){
 }
 
 function pushar(e){
-  if(e.keyCode == 65)
+  if(e.keyCode === 65)
     camara.position.z++;
-  else if(e.keyCode == 81)
+  else if(e.keyCode === 81)
     camara.position.z--;
 }
 
