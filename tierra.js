88dee5c913b8f3a01f533c367151908b2c8691b6
LuisEstradaB <l.f.e.b.220395@hotmail.com>
2017-06-01 14:12:12 -0500

    Update tierra.js

--- tierra.js
+++ tierra.js
@@ -13,8 +13,6 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    
-    var controls = new THREE.OrbitControls( camera );
 
     scene = new THREE.Scene();
  
